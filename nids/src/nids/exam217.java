094b5f24b94b13cd0e6ea5434f9997fc8aba0f0b
schub <schub@NITRO>
2024-02-09 18:17:12 +0530

    fuckgod

---
+++ nids/src/nids/exam217.java
@@ -0,0 +1,13 @@
+package nids;
+
+public class exam217 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	  System.out.println("hello world");
+	  
+		
+	}
+
+}
